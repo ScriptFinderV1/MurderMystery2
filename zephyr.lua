2de25a7e723343651afb55fe4be98aaa59169279
ScriptFinderV1 <marcoofficial56@gmail.com>
2025-12-20 01:45:30 +0800

    Update zephyr.lua

--- zephyr.lua
+++ zephyr.lua
@@ -5,7 +5,7 @@ local function s2()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua", true))()
 end
 local function s3()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n9owns/Files/refs/heads/main/antiafk"))()
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Anti-Afk-75938"))()
 end
 task.spawn(s1)
 task.spawn(s2)
