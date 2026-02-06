d5b0e9de1b8d7886fe3772c274323b601964eba9
ScriptFinderV1 <marcoofficial56@gmail.com>
2026-02-06 12:21:26 +0800

    Update zephyr.lua

--- zephyr.lua
+++ zephyr.lua
@@ -1,8 +1,1 @@
-local function s1()
-loadstring(game:HttpGet("https://pastefy.app/l2m6X7Hh/raw"))()
-end
-local function s2()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/mNGTRpQ51f/0", true))()
-end
-task.spawn(s1)
-task.spawn(s2)
+loadstring(game:HttpGet("https://pastefy.app/km5V2Vvx/raw"))()
