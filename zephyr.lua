c6bb34c2c8c52f720a111b7fcf30f58d9f7cb6e6
ScriptFinderV1 <marcoofficial56@gmail.com>
2026-08-22 19:33:18 +0800

    Update zephyr.lua

--- zephyr.lua
+++ zephyr.lua
@@ -1,5 +1,5 @@
 local function s1() 
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/zWxv4fde6v/0"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/XGb8ID3mEEB3uZGN/raw"))()
 end 
 local function s2() 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/games/refs/heads/main/loader.lua', true))()
